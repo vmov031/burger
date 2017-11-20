4b7b59475aedcf5672e30111202d339bfebfc2f8
Vanessa Montero <vanessamontero@vanessas-mbp.attlocal.net>
2017-11-20 00:24:18 -0800

    adding

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (
+id int AUTO_INCREMENT,
+burger_name VARCHAR(100),
+devoured BIT DEFAULT 1 NOT NULL,
+date TIMESTAMP NOT NULL DEFAULT(GETDATE()),
+PRIMARY KEY(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers(burger_name, devoured)
+VALUES("The Hook", 0), ("Love Triangle", 1), ("Heart Attack", 1);
